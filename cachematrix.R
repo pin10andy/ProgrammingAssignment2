f2752df4882da9493d9c195e39e71e74cb0016b2
AndyChou <pin10andy@gmail.com>
2014-07-26 21:49:10 +0800

    Data science assignment#2
    Edit by Chou_Tai

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        set <- function(y){
+                m = NULL
+                x <<- y
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+        
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        if(!is.null(getsolve)){
+                message("getting chached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data)
+        x$setsolve(s)
+        s
+}        
+
+## Return a matrix that is the inverse of 'x'
